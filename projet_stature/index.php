d9f8ab974250521c2222a55cd0c7ca8f2160c4b2
WilbersonAlexis <142346630+WilbersonAlexis@users.noreply.github.com>
2023-08-15 14:13:31 -0700

    Add files via upload

---
+++ projet_stature/index.php
@@ -0,0 +1,307 @@
+<?php
+require"heath.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon stature</title>
+</head>
+<body>
+
+  <center>
+    <h2>Personal details</h2>
+    
+<div class="mage">
+<img src="images/alex.png" class="rounded-circle" alt="" height="200px" width="200px"  >
+
+<h4> ALEXIS Wilberson</h4>
+</div>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Checkout example · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    
+<div class="container">
+  <main>
+    
+
+   
+            
+
+      <div class="col-md-7 col-lg-8">
+        
+        <form class="needs-validation" novalidate>
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="firstName" class="form-label">First name</label>
+              <input type="text" class="form-control" id="firstName" placeholder="Wilberson" value="" required style="font-size: 20px;" disabled>
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="lastName" class="form-label">Last name</label>
+              <input type="text" class="form-control" id="lastName" placeholder="ALEXIS" value="" required  style="font-size: 20px;" disabled>
+              <div class="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="username" class="form-label">phone number</label>
+              <div class="input-group has-validation">
+                <img src="images/call.png" alt="" width="45px" height="70px">
+                <input type="text" class="form-control" id="username" placeholder="+(509) 36953382" required style="font-size: 20px;" disabled>
+              <div class="invalid-feedback">
+                  Your username is required.
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12">
+          
+              <label for="email" class="form-label">Email <span class="text-muted"></span></label>
+              <input type="email" class="form-control" id="email" placeholder="alexiswilberson8@gmail.com" style="font-size: 20px;" disabled>
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address" class="form-label">Address</label>
+              <input type="text" class="form-control" id="address" placeholder="jeremie, grand'anse, HAITI"  Web developer style="font-size: 20px;" disabled>
+              <div class="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address2" class="form-label">profession </label>
+              <input type="text" class="form-control" id="address2" placeholder="Web developer and Big data" style="font-size: 20px;" disabled>
+            </div>
+
+            <div class="col-md-5">
+              <label for="country" class="form-label">Sex</label>
+              <input type="text" class="form-control" id="address" placeholder="Male" style="font-size: 20px;"disabled>
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid country.
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <label for="state" class="form-label">speak</label>
+              <input type="text" class="form-control" id="address" placeholder="creole, French, English" style="font-size: 20px;" disabled>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+
+           
+
+          <hr class="my-4">
+
+          
+
+          
+
+          <hr class="my-4">
+
+         
+
+          
+
+          
+            </div>
+
+           
+
+            
+
+            <div class="text-success">
+                
+                <a href="https://wa.me/50936953382" target="_blank">
+                  <img src="image_go/what.png" alt="" height="60px" width="60px" >
+                </a>
+                
+                  
+                <a class="face" href="https://www.facebook.com/profile.php?id=100076056071231" target="_blank">
+                  <img src="image_go/face.jpg" alt="Facebook" height="50px" width="60px" >
+                </a>
+                
+                            </div>
+            </div>
+          </div>
+
+
+          
+        </form>
+      </div>
+    </div>
+  </main>
+
+  
+</div>
+
+
+    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+      <script src="form-validation.js"></script>
+  </body>
+</html>
+</center>
+ 
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+
+
+  
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+
+<style>
+  .mage{
+    margin-top: 10px;
+    color: white;
+    
+    
+
+    
+  }
+  h2{
+    margin-top: 70px;
+  }
+  .contenair{
+    border: 5px solid #000;
+  padding: 20px;
+  padding: 20px;
+    margin-top: 30px;
+    margin-left: 400px;
+    margin-right: 300px;
+    border-color: gray;
+    
+    
+
+    
+
+    
+    
+    
+  }
+.mb-3{
+  border: 2px solid #000;
+  padding: 20px;
+  background-color: #f8f8f8;;
+  height: 70px;
+  width: 550px;
+  font-size: 20px;
+  
+  
+}
+body{
+  background-image:url(Lab.jpg) ;
+  
+  background-repeat: no-repeat;
+  background-size: cover;
+  
+  
+
+}
+.insetaname{
+  margin-left: 250px;
+  margin-top: -30px;
+}
+.inseta{
+  margin-left: 150px;
+  margin-top: -24px;
+}
+
+h4{
+  color: black;
+}
+.form-label{
+  color: black;
+  font-size: 20px;
+}
+.container{
+  margin-top: 110px;
+   background-color: white; 
+
+}
+.face{
+  margin-left: 40px;
+}
+
+
+   
+</style>
+</body>
+</html>
